2313414419f85b9998f9d7eaaf0b0dfd60a6ff4e
Thiago Cunha <thiagocunha.java@gmail.com>
2016-10-16 03:32:53 -0200

    Adicionando quebra de linha

--- programa.php
+++ programa.php
@@ -2,7 +2,7 @@
      date_default_timezone_set('America/Sao_Paulo');
      echo "Hoje é dia " . date('d/m/y l') . "<br />";
      echo "Agora são " . date('H:i:s A') . "<br />";
-     echo "Faltam " . (6 - date('w')) . " para o próximo sábado."
+     echo "Faltam " . (6 - date('w')) . " para o próximo sábado.<br />"
 
 
 ?>
